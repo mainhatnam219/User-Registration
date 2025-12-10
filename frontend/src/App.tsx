6f955ff6f50607e5580e254254c14f0e8f46547d
mainhatnam219 <22120219@student.hcmus.edu.vn>
2025-12-10 11:10:36 +0700

    add react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Home, Login, SignUp, Dashboard } from '@/pages';
 import { ProtectedRoute } from '@/components/ProtectedRoute';
 
@@ -25,6 +26,7 @@ export const App: React.FC = () => {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
